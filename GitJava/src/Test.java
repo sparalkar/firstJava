edf2bb9b2dcf5cfe1a2d98e20192c276a17514c1
Swati <sparalkar@valethi.com>
2015-04-02 16:26:53 +0530

    fffff

--- GitJava/src/Test.java
+++ GitJava/src/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("=-----Hi this is swati t   est project commited on behalf of KAustubh----");
+		System.out.println("= -----Hi this is swati t   est project commited on behalf of KAustubh----");
 	}
 
 }
